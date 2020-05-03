da58cb1a3f214d08170668fd2136925b2b23326a
Harsha Vardhan M N <36810886+HarshaV23@users.noreply.github.com>
2020-05-03 18:49:31 +0530

    Second Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SlideshowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
