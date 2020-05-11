fd359c4a12057cb2fb55c0942bdd5ceec6dcb3a5
Harsha Vardhan M N <36810886+HarshaV23@users.noreply.github.com>
2020-05-11 13:36:35 +0530

    added search bar in home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  serviceName:string;
+  responseBody:any;
+  constructor(private httpRef:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+  searchNames(){ 
+    this.httpRef.get("http://localhost:5003//service/serviceByName/"+this.serviceName).subscribe((responseBody)=>{
+      this.responseBody=responseBody;
+      console.log(responseBody);
+    });
+  }
+}
